cf3153d58af1a5c641b4378cd6af1a30ddf05b17
sebastian <sebastianserri@gmail.com>
2018-08-09 19:59:46 -0300

    adding files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import Router from "./components/Router";
+import "./css/style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Router/>, document.querySelector('#main'));
