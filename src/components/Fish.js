163bf05aa4432e9fe022e3a154d7cc5fe0064820
sebastian <sebastianserri@gmail.com>
2018-08-11 23:55:02 -0300

    Adding PropTypes Validations

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+  static propTypes = {
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number,
+      desc: PropTypes.string
+    }),
+    addToOrder: PropTypes.func
+  };
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
     const isAvailable = status === "available";
@@ -22,6 +34,6 @@ class Fish extends React.Component {
       </li>
     );
   }
-}
+};
 
 export default Fish;
