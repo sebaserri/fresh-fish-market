bb81d5a9cfd739992a40d458fc894fdc56a30970
sebastian <sebastianserri@gmail.com>
2018-08-11 23:25:15 -0300

    Adding attributes

--- src/base.js
+++ src/base.js
@@ -2,7 +2,12 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 });
 
 const base = Rebase.createClass(firebase.database()); 
